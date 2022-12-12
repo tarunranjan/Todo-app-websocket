71d4e9a0fea3fd84e273883035bbe7001147d33b
tarunranjan <68010132+tarunranjan@users.noreply.github.com>
2022-12-12 23:52:19 +0530

    Delete server-postgres-cluster directory

--- server-postgres-cluster/lib/todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const todoSchema = new mongoose.Schema({
-    "title" : String
-});
-
-const todos = mongoose.model('todos',todoSchema);
-
-module.exports = todos;
